76af4933c4dc10fb50d462e5f8538871871879a9
huangjin <519730485@qq.com>
2022-05-07 15:27:48 +0800

    init

---
+++ src/App.js
@@ -0,0 +1,14 @@
+export function App() {
+  return (
+    <h1>
+      <ul>
+        <li>
+          <a href="/html/amy_installed.html">Amy Install Preview Page</a>
+        </li>
+        <li>
+          <a href="/html/test.html">Test</a>
+        </li>
+      </ul>
+    </h1>
+  )
+}
