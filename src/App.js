07479b66f6e1fed01a5af7df828234e07d5f54b3
huangjin <519730485@qq.com>
2022-05-09 17:06:07 +0800

    iniit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,22 @@
+import { useEffect } from 'react'
+import axios from 'axios'
+
 export function App() {
+  useEffect(() => {
+    axios
+      .post('https://amysignup.testing.comm100dev.io/api/amy/login/thirdParty', {
+        email: 'huangjincq@gmai2l.com',
+        thirdPartyToken: 'xxxx',
+        thirdPartyType: 0,
+      })
+      .then(function (response) {
+        console.log(response)
+      })
+      .catch(function (error) {
+        console.log(error)
+      })
+  }, [])
+
   return (
     <h1>
       <ul>
