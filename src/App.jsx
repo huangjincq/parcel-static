f6c3e580e9002927ca23bdf02bee79eb83a9c733
aking <huangjin@xx.com>
2022-12-13 14:59:11 +0800

    feat:移除

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,5 @@
-import ReactDOM from 'react-dom/client'
-
 import React from 'react'
-import TwoFa from './components/TwoFa'
-import reactToWebComponent from 'react-to-webcomponent'
 
 export function App() {
-  return <h1>{/* <Counter /> */}</h1>
+  return <h1>xxx</h1>
 }
-
-customElements.define('two-fa', reactToWebComponent(TwoFa, React, ReactDOM))
--- two-fa.js
+++
@@ -1,7 +0,0 @@
-import ReactDOM from 'react-dom/client'
-
-import React from 'react'
-import TwoFa from './src/components/TwoFa'
-import reactToWebComponent from 'react-to-webcomponent'
-
-customElements.define('two-fa', reactToWebComponent(TwoFa, React, ReactDOM))
