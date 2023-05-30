68112ce81d1dfefeaf849c4fbd8de34b2bdbf312
huangjin2 <huangjin2@webull.com>
2023-05-30 09:58:11 +0800

    feat: up date

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,86 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react';
+import { Graph } from '@antv/x6';
+import './index.scss';
+import createData from './createData';
+import { DagreLayout } from '@antv/layout';
+
+const data = {
+  nodes: [
+    {
+      id: 'node1',
+      shape: 'rect',
+      x: 40,
+      y: 40,
+      width: 100,
+      height: 40,
+      label: 'hello',
+    },
+    {
+      id: 'node2',
+      shape: 'rect',
+
+      width: 100,
+      height: 40,
+      label: 'world',
+    },
+  ],
+  edges: [
+    {
+      shape: 'edge',
+      source: 'node1',
+      target: 'node2',
+    },
+  ],
+};
 
 export function App() {
-  return <h1>xxx</h1>
+  const refContainer = useRef();
+
+  useEffect(() => {
+    const graph = new Graph({
+      container: refContainer.current, // 网格
+      // 网格
+      grid: true,
+      // 剪切板
+      clipboard: true,
+      // 键盘快捷键
+      keyboard: true,
+      // 撤销/重做
+      history: true,
+      panning: {
+        enabled: true,
+        eventTypes: ['leftMouseDown', 'mouseWheel'],
+      },
+      mousewheel: {
+        enabled: true,
+        zoomAtMousePosition: true,
+        // modifiers: 'ctrl',
+        minScale: 0.1,
+        maxScale: 1,
+      },
+      // 设置画布背景颜色
+      background: {
+        color: '#F2F7FA',
+      },
+      autoResize: true,
+    });
+    // 自动 dagre 布局
+    const dagreLayout = new DagreLayout({
+      type: 'dagre',
+      rankdir: 'LR',
+      align: 'UR',
+      ranksep: 100,
+      nodesep: 15,
+    });
+    const model = dagreLayout.layout(createData);
+
+    graph.fromJSON(model); // 渲染元素
+    graph.centerContent(); // 居中显示
+  }, []);
+
+  return (
+    <div className="helloworld-app">
+      <div className="app-content" ref={refContainer}></div>
+    </div>
+  );
 }
---
+++ src/createData.js
@@ -0,0 +1,51 @@
+import mockData from './mockData.json';
+
+const createData = (data) => {
+  const nodes = data.map((v, i) => {
+    const label = v.title || v.taskId;
+    return {
+      shape: 'rect',
+      color: '#1890FF',
+      label: label,
+
+      width: getTextWith(label),
+      height: 40,
+      id: v.taskId,
+    };
+  });
+
+  const edges = [];
+  // 构建节点路径数据
+  data.forEach((taskItem) => {
+    taskItem.downstreamTaskIdList.forEach((taskId) => {
+      if (data.some((v) => v.taskId === taskId)) {
+        edges.push({
+          source: taskItem.taskId,
+          target: taskId,
+          shape: 'edge',
+        });
+      }
+    });
+  });
+
+  return {
+    nodes,
+    edges,
+  };
+};
+
+/**
+ * 根据文本内容计算文本长度
+ * @param text 文本内容
+ * @param fontStyle 文本样式
+ * @returns length
+ */
+export const getTextWith = (text, fontStyle = '14px Arial, PingFang SC, PingFang SC Regular, sans-serif') => {
+  const canvas = document.createElement('canvas');
+  const context = canvas.getContext('2d');
+  context.font = fontStyle;
+  const dimension = context.measureText(text);
+  return dimension.width;
+};
+
+export default createData(mockData);
